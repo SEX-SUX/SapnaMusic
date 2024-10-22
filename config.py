df30d7afa96f815bca4a21e8347e245c2c4ee0bf
SEX-SUX <sachinxpapa@gmail.com>
2024-10-22 21:16:29 +0530

    sᴀᴘɴᴀ ᴠ ~ 1.0

--- config.py
+++ config.py
@@ -38,8 +38,8 @@ UPSTREAM_REPO = getenv(
     "https://github.com/saiboks/NexaHaHa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-    GIT_TOKEN = getenv(
-    "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", None
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/novauibot")
