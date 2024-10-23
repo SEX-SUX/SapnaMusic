537a48e5b440dc709555f0835d04449927f55c16
SEX-SUX <sachinxpapa@gmail.com>
2024-10-23 18:43:56 +0530

    sᴀᴘɴᴀ ᴠ ~ 1.0

--- SapnaMusic/help/buttons.py
+++ SapnaMusic/help/buttons.py
@@ -1,1 +1,34 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters, enums 
 
+import config
+from SACHIN_MUSIC import app
+
+class BUTTONS(object):
+    ABUTTON = [
+        [
+            InlineKeyboardButton("ᴀɪ | ᴄʜᴀᴛɢᴘᴛ", callback_data="AI_BACK HELP_01"),
+        ],
+        [
+            InlineKeyboardButton("ᴄᴀʀʙᴏɴ", callback_data="AI_BACK HELP_02"),
+            InlineKeyboardButton("ᴛᴛs", callback_data="AI_BACK HELP_03"),
+            InlineKeyboardButton("ɪɴғᴏ", callback_data="AI_BACK HELP_04"),
+        ],
+        [
+            InlineKeyboardButton("ʙᴀɴ", callback_data="AI_BACK HELP_05"),
+            InlineKeyboardButton("ᴍᴜᴛᴇ", callback_data="AI_BACK HELP_06"),
+            InlineKeyboardButton("ᴋɪᴄᴋ", callback_data="AI_BACK HELP_07"),
+        ],
+        [
+            InlineKeyboardButton("ɢʀᴀᴘʜ", callback_data="AI_BACK HELP_08"),
+            InlineKeyboardButton("sɢ", callback_data="AI_BACK HELP_09"),
+            InlineKeyboardButton("ᴡᴇʟᴄᴏᴍᴇ", callback_data="AI_BACK HELP_10"),
+        ],
+        [
+            InlineKeyboardButton("ᴢᴏᴍʙɪᴇs", callback_data="AI_BACK HELP_11"),
+            InlineKeyboardButton("ɢ-ʙʏᴇ", callback_data="AI_BACK HELP_12"),
+            InlineKeyboardButton("ᴍᴀꜱꜱ ᴀᴄᴛɪᴏɴ", callback_data="AI_BACK HELP_13"), 
+        ],
+        [
+            InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"settingsback_helper"),]
+        ]
